4b42b22098246bf5e5fd43712966530a8e657f73
anhTeddy <nguyentuananh0610@gmail.com>
2016-06-16 16:09:46 +0700

    added posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601110434) do
+ActiveRecord::Schema.define(version: 20160616040418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string  "cat_name"
+    t.string  "title"
+    t.string  "content"
+    t.string  "image_url"
+    t.string  "up"
+    t.string  "down"
+    t.string  "creator_id"
+    t.string  "result"
+    t.integer "type"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
