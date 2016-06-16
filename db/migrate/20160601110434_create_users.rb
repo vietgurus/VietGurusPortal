676993f8d23cb33f31c5924447426bbcf8bde209
anhTeddy <nguyentuananh0610@gmail.com>
2016-06-16 15:14:49 +0700

    integrate user table

--- db/migrate/20160601110434_create_users.rb
+++ db/migrate/20160601110434_create_users.rb
@@ -3,14 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.integer :role
-      t.string :password_digest
-      t.string :auth_code
-      t.references :sales_manager, index: true
-      t.references :country_manager, index: true
-      t.string :slug
-
-      t.timestamps null: false
+      t.string :avatar
     end
   end
 end
