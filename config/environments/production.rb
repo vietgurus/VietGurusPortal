3d2ae16acd94fbb8143281cc7add03417ee83ab8
Khoi <vuminhkhoi@gmail.com>
2016-06-27 09:50:03 +0700

    Removed mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # TODO remove when deploy to heroku
+  config.serve_static_assets = true
+
 end
